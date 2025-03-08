cc8b883e317074cb54c6c43c13a6ff85a2e63689
Rowland <rowland.h.mosbergen@gmail.com>
2025-03-08 12:17:15 +1100

    adding gitignore

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <PersonsTable />
-  </div>
-  <div>
     <p>
     <nuxt-link to="/about">Go to About Page</nuxt-link>
     </p>
     <p>
     <nuxt-link to="/personForm">Go to Add a Person</nuxt-link>
     </p>
+  </div>
+  <div>
+    <PersonsTable />
   </div>
 </template>
 
--- pages/persons/[id].vue
+++ pages/persons/[id].vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1><nuxt-link to="/">Home</nuxt-link></h1>
     <h1>Person Details</h1>
     <p>Person ID: {{ id }}</p>
 
